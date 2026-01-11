283a651619f0c96b7095802e61368851bfe42e2c
Sebastian337 <aleshka_demidov@mail.ru>
2026-01-12 00:06:35 +0300

    add auxiliary module and requirements

---
+++ helpers.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from urllib.parse import urlparse, unquote
+
+
+def download_image(url, filepath, params=None):
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+    
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+
+def get_file_extension_from_url(url):
+    parsed_url = urlparse(url)
+    file_path = unquote(parsed_url.path)
+    file_name = os.path.split(file_path)[1]
+    file_extension = os.path.splitext(file_name)[1]
+    return file_extension
