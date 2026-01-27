315aefcebbadf79c23ead3f65834bd03c478ef12
Sebastian337 <aleshka_demidov@mail.ru>
2026-01-27 22:29:12 +0300

    Update publish_all_photos.py

--- publish_all_photos.py
+++ publish_all_photos.py
@@ -1,30 +1,8 @@
-import os
 import random
 import time
-import requests
 from dotenv import load_dotenv
-
-
-def get_all_images(images_dir='images'):
-    all_images = []
-    for root, dirs, files in os.walk(images_dir):
-        for file in files:
-            if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
-                all_images.append(os.path.join(root, file))
-    return all_images
-
-
-def send_telegram_photo(bot_token, chat_id, image_path):
-    url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
-    
-    with open(image_path, 'rb') as photo_file:
-        photo_data = photo_file.read()
-    
-    files = {'photo': ('photo.jpg', photo_data)}
-    data = {'chat_id': chat_id}
-    
-    response = requests.post(url, files=files, data=data, timeout=30)
-    response.raise_for_status()
+import os
+from telegram_tools import get_all_images, send_telegram_photo
 
 
 def publish_photos_loop(bot_token, chat_id, delay_hours):
@@ -78,5 +56,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
